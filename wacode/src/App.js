70cb6230ac97862d853a8df627d058e2e4675979
MarkFuller1 <mark_fuller1@baylor.edu>
2020-02-22 10:37:11 -0600

    here

--- wacode/src/App.js
+++ wacode/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <h1>sup</h1>
+    <Page/>
   );
 }
 
